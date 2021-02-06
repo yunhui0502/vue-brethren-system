ae12df76ae521144dada9b10ea1bbd201269a74e
yunhui0502 <2661234231@qq.com>
2021-02-06 17:05:06 +0800

    第2期完成

--- src/components/page/project-center/project-center.vue
+++ src/components/page/project-center/project-center.vue
@@ -8,7 +8,7 @@
                 <el-button type="primary" style="float: right; margin: 20px" @click="add">添加</el-button>
             </div>
             <el-table
-                :data="tableData.slice((query.pageIndex-1)*query.pageSize,query.pageIndex*query.pageSize)"
+                :data="tableData.slice((query.pageIndex - 1) * query.pageSize, query.pageIndex * query.pageSize)"
                 border
                 class="table"
                 ref="multipleTable"
@@ -17,13 +17,14 @@
             >
                 <el-table-column prop="projectName" align="center" label="项目名称"></el-table-column>
                 <el-table-column prop="address" align="center" label="开发商">
-                    <!-- <template slot-scope="scope">
-                        <div v-for="(item, i) in scope.row.landType.土地名称" :key="i">{{ item }}</div>
-                    </template> -->
+                    <template slot-scope="scope">
+                        <div v-for="(item, i) in scope.row.developers" :key="i">{{ item }}</div>
+                    </template>
                 </el-table-column>
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
                         <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                        <el-button type="text" icon="el-icon-edit" @click="examine(scope.row)">查看</el-button>
                         <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)"
                             >删除</el-button
                         >
@@ -51,6 +52,27 @@
                 <!-- <el-form-item label="开发商名称">
                     <el-input v-model="form.developersName"></el-input>
                 </el-form-item> -->
+                <!-- <el-form-item label="所以土地/楼盘">
+                    <el-table
+                        :data="selectPremisesData"
+                        border
+                        class="table"
+                        ref="multipleTable"
+                        header-cell-class-name="table-header"
+                        @selection-change="handleSelectionChange"
+                    >
+                        <el-table-column prop="landName" align="center" label="土地名称"></el-table-column>
+                        <el-table-column prop="premisesName" align="center" label="楼盘名称"></el-table-column>
+                    </el-table>
+                </el-form-item> -->
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog :title="title" :visible.sync="editVisible2" width="50%" center>
+            <el-form ref="form" :model="form" label-width="100px">
                 <el-form-item label="所以土地/楼盘">
                     <el-table
                         :data="selectPremisesData"
@@ -66,7 +88,7 @@
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button @click="editVisible2 = false">取 消</el-button>
                 <el-button type="primary" @click="saveEdit">确 定</el-button>
             </span>
         </el-dialog>
@@ -81,7 +103,8 @@ export default {
     data() {
         return {
             title: '',
-            selectPremisesData:'',
+            selectPremisesData: '',
+            Premisesarr: [],
             query: {
                 address: '',
                 name: '',
@@ -96,6 +119,7 @@ export default {
             multipleSelection: [],
             delList: [],
             editVisible: false,
+            editVisible2: false,
             pageTotal: 0,
             idx: -1,
             id: -1
@@ -103,13 +127,27 @@ export default {
     },
     created() {
         this.getData();
-        this.selectPremises();
+        // this.selectPremises();
     },
     methods: {
-        selectPremises() {
-            userApi.selectPremises({}, (res) => {
+        examine(row) {
+            this.editVisible2 = true;
+             userApi.selectPremises({projectId: row.projectId }, (res) => {
                 console.log('楼盘', res);
                 this.selectPremisesData = res.data.data;
+            });
+        },
+
+        developers() {
+            this.tableData.forEach((item) => {
+                item.developers = [];
+                userApi.selectPremises({ projectId: item.projectId }, (res) => {
+                    console.log('楼盘', res);
+                    let data = res.data.data;
+                    data.forEach((item2) => {
+                        item.developers.push(item2.developersName);
+                    });
+                });
             });
         },
         add() {
@@ -123,16 +161,16 @@ export default {
             //     this.tableData = res.list;
             //     this.pageTotal = res.pageTotal || 50;
             // });
-            let Obj = {
-                
-            }
+            let Obj = {};
             if (this.query.name != '') {
-                Obj.projectName = this.query.name
+                Obj.projectName = this.query.name;
             }
-            userApi.selectProject(Obj,(res) => {
+            userApi.selectProject(Obj, (res) => {
                 console.log(res);
                 this.tableData = res.data.data;
                 this.pageTotal = this.tableData.length;
+                this.developers();
+                console.log('this.tableData', this.tableData);
             });
         },
         // 添加
